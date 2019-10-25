925dd01fd6bf3ab8e9df674f452fba20911fddf5
swyx <shawnthe1@gmail.com>
2019-10-24 23:33:44 -0400

    Update pics.js

--- functions/pics/pics.js
+++ functions/pics/pics.js
@@ -12,20 +12,20 @@ exports.handler = async (event, context) => {
       headers,
       body: JSON.stringify(
         [
-          "https://images.dog.ceo/breeds/shiba/shiba-1.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-10.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-13.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
-          "https://images.dog.ceo/breeds/shiba/shiba-7.JPG",
-          "https://images.dog.ceo/breeds/shiba/shiba-8.JPG",
-          "https://images.dog.ceo/breeds/shiba/shiba-9.jpg"
+    "https://images.dog.ceo/breeds/shiba/shiba-1.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-10.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-13.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg"
         ].slice(-1 * count)
       )
     }
